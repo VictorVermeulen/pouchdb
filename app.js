38cb4b9b61e1f611eaf031892daca3b497603bdb
Victor Vermeulen <vvermeulen@Victors-MacBook-Pro.local>
2020-07-10 14:23:22 +0200

    added buttons danger zone

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 var db = new PouchDB("kittens", { adapter: "worker" });
-var docs = new Array();
-for (i = 0; i < 1000000; i++) {
-  var doc = {
-    _id: "keyx" + i,
-    name: "name",
-  };
-  docs[i] = doc;
+
+function getRandomNumber() {
+  return Math.random();
 }
-db.bulkDocs(docs)
-  .then(function (res) {})
-  .catch(function (err) {
-    console.log(err);
-  });
+
+function Add10Entries(amount) {
+  var docs = new Array();
+  for (let i = 0; i < amount; i++) {
+    var doc = {
+      _id: "keyx" + getRandomNumber(),
+      name: "name",
+    };
+    docs[i] = doc;
+  }
+  db.bulkDocs(docs)
+    .then(function (res) {
+      console.log(res, "added data - response");
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+async function GetAllEntries(amount) {
+  console.log(await db.allDocs());
+}
+
+async function DeleteAllDocs() {
+  const allDocs = await db.allDocs();
+  for (let i = 0; i < allDocs.total_rows; i++) {
+    await db.remove(allDocs.rows[i].id, allDocs.rows[i].value.rev);
+  }
+  console.log("Deleted all docs");
+}
