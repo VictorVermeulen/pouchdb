9016540e3e47f973ba3c846cc94684534e9071aa
Victor Vermeulen <vvermeulen@Victors-MacBook-Pro.local>
2020-07-10 09:37:58 +0200

    second commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pouchdb
+
+You can see this great site in action here: https://frosty-pike-bf6148.netlify.app/
---
+++ app.js
@@ -0,0 +1,14 @@
+var db = new PouchDB("kittens");
+var docs = new Array();
+for (i = 0; i < 1000000; i++) {
+  var doc = {
+    _id: "keyx" + i,
+    name: "name",
+  };
+  docs[i] = doc;
+}
+db.bulkDocs(docs)
+  .then(function(res) {})
+  .catch(function(err) {
+    console.log(err);
+  });
