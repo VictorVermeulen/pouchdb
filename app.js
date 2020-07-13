4bcfbc4c605f0510b358dae414fda65a4099c327
Alan <aendersoy@enrise.com>
2020-07-13 15:25:28 +0200

    make buttons work too

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const worker = new Worker('./worker.js');
 
 function buttonClicked(number) {
-
   worker.postMessage({ number });
-  console.log('Message posted to worker');
-}
-
-worker.onmessage = function(e) {
-  result.textContent = e.data;
-  console.log('Message received from worker');
+  console.log(`Posted task with ${number} entries to worker...`);
 }
--- worker.js
+++ worker.js
@@ -1,7 +1,6 @@
 importScripts('./pouchdb-7.2.1.js');
 
-onmessage = function(e) {
-  
+onmessage = async function(e) {  
   const db = new PouchDB("kittens");
   const number = e.data.number;
 
@@ -9,21 +8,36 @@ onmessage = function(e) {
     return Math.random();
   }
 
-  let docs = new Array();
+  switch (number) {
+    case 'getAll':
+      console.log(await db.allDocs());
 
-  for (let i = 0; i < number; i++) {
-    docs[i] = {
-      _id: "keyx" + getRandomNumber(),
-      name: "name",
-    };
+      break;
+    case 'deleteAll': 
+      const allDocs = await db.allDocs();
+      
+      for (let i = 0; i < allDocs.total_rows; i++) {
+        await db.remove(allDocs.rows[i].id, allDocs.rows[i].value.rev);
+      }
+      console.log("Deleted all docs");
+
+      break;
+    default:
+      let docs = new Array();
+
+      for (let i = 0; i < number; i++) {
+        docs[i] = {
+          _id: "keyx" + getRandomNumber(),
+          name: "name",
+        };
+      }
+    
+      db.bulkDocs(docs).then(function (res) {
+        console.log(res, "added data - response");
+      }).catch(function (err) {
+        console.log(err);
+      });
+
+      break;
   }
-
-  db.bulkDocs(docs)
-    .then(function (res) {
-      console.log(res, "added data - response");
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-
 }
